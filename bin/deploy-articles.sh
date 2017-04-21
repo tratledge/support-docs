3d7253b6fad10a9b0f50c09b601993a13c6fe7ab
Rich Leland <rich.leland@sparkpost.com>
2017-04-21 11:12:58 -0400

    Push in post excerpts

--- bin/deploy-articles.sh
+++ bin/deploy-articles.sh
@@ -76,6 +76,7 @@ for filepath in "${CHANGED_FILES[@]}"; do
   md_post=$(node bin/markdown.js "$filepath")
   md_post_images=("$(echo $md_post | jq --compact-output --raw-output '.images')")
   md_post_title="$(echo $md_post | jq '.meta.title' --raw-output)"
+  md_post_excerpt="$(echo $md_post | jq '.meta.description' --raw-output)"
 
   # create
   if [ -n "$md_post" ] && [ "-1" == "$wp_post_index" ]; then
@@ -94,7 +95,7 @@ for filepath in "${CHANGED_FILES[@]}"; do
 
       md_post_content=$(generate_html "$filepath" "${imported_image_ids[@]}")
 
-      echo " - $(do_wp post update $wp_post_id --post_content="$md_post_content")"
+      echo " - $(do_wp post update $wp_post_id --post_content="$md_post_content" --post_excerpt="$md_post_excerpt")"
     else
       echo " - $wp_post_id"
     fi
@@ -115,7 +116,7 @@ for filepath in "${CHANGED_FILES[@]}"; do
 
     md_post_content=$(generate_html "$filepath" "${imported_image_ids[@]}")
 
-    echo " - $(do_wp post update $wp_post_id --post_title="$md_post_title" --post_content="$md_post_content")"
+    echo " - $(do_wp post update $wp_post_id --post_title="$md_post_title" --post_content="$md_post_content" --post_excerpt="$md_post_excerpt")"
   fi
 
   # delete
