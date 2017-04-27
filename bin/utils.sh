876c77cafdd982adc8b8a94d4d05a3756330f50b
Rich Leland <rich.leland@sparkpost.com>
2017-04-27 13:08:52 -0400

    More debug tweaks

--- bin/utils.sh
+++ bin/utils.sh
@@ -20,7 +20,8 @@ if [ "$bamboo_planRepository_branchName" == "master" ]; then
   echo "changed files: ${CHANGED_FILES[@]}"
 else
   echo "it is $bamboo_planRepository_branchName"
-  CHANGED_FILES=(`git diff --name-only master...$bamboo_planRepository_revision -- ./articles`)
+  git diff --name-only master...$bamboo_planRepository_revision -- ./articles
+  CHANGED_FILES=($(git diff --name-only master...$bamboo_planRepository_revision -- ./articles))
   echo "changed files: ${CHANGED_FILES[@]}"
 fi
 
